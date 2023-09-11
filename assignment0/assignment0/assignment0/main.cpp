ab087c892b147b7213a27f266e186a19c571e73f
siyanhu <siyan.dev@gmail.com>
2023-09-11 13:00:58 +0800

    finish task 1

--- assignment0/assignment0/assignment0/main.cpp
+++ assignment0/assignment0/assignment0/main.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <random>
 #include "vecmath.h"
 using namespace std;
 
@@ -24,66 +25,36 @@ vector<Vector3f> vecn;
 // This is the list of faces (indices into vecv and vecn)
 vector<vector<unsigned> > vecf;
 
-
+#pragma mark - Editting Area
 // You will need more global variables to implement color and position changes
+float get_a_random_indicator(void) {
+    random_device device;
+    mt19937 random_num(device());
+    uniform_int_distribution<mt19937::result_type> dist_color(0, 255);
+    int int_indicator = dist_color(random_num);
+    //(x - min) / (max - min)
+    float normalised_indicator = (float)int_indicator/255.0;
+    return normalised_indicator;
+}
 
+float *update_color_with_a_random_value(void) {
+    float indicator_first = get_a_random_indicator();
+    float indicator_second = get_a_random_indicator();
+    float indicator_third = get_a_random_indicator();
+    float indicator_forth = get_a_random_indicator();
+//    cout<<"Next Color: "<<indicator_first<<","<<indicator_second<<","<<indicator_third<<","<<indicator_forth<< endl;
+    float r[4] = {indicator_first, indicator_second, indicator_third, indicator_forth};
+    return r;
+}
 
-// These are convenience functions which allow us to call OpenGL 
+#pragma mark - Fixed Functions
+// These are convenience functions which allow us to call OpenGL
 // methods on Vec3d objects
-inline void glVertex(const Vector3f &a) 
+inline void glVertex(const Vector3f &a)
 { glVertex3fv(a); }
 
-inline void glNormal(const Vector3f &a) 
+inline void glNormal(const Vector3f &a)
 { glNormal3fv(a); }
-
-
-// This function is called whenever a "Normal" key press is received.
-void keyboardFunc( unsigned char key, int x, int y )
-{
-    switch ( key )
-    {
-    case 27: // Escape key
-        exit(0);
-        break;
-    case 'c':
-        // add code to change color here
-		cout << "Unhandled key press " << key << "." << endl; 
-        break;
-    default:
-        cout << "Unhandled key press " << key << "." << endl;        
-    }
-
-	// this will refresh the screen so that the user sees the color change
-    glutPostRedisplay();
-}
-
-// This function is called whenever a "Special" key press is received.
-// Right now, it's handling the arrow keys.
-void specialFunc( int key, int x, int y )
-{
-    switch ( key )
-    {
-    case GLUT_KEY_UP:
-        // add code to change light position
-		cout << "Unhandled key press: up arrow." << endl;
-		break;
-    case GLUT_KEY_DOWN:
-        // add code to change light position
-		cout << "Unhandled key press: down arrow." << endl;
-		break;
-    case GLUT_KEY_LEFT:
-        // add code to change light position
-		cout << "Unhandled key press: left arrow." << endl;
-		break;
-    case GLUT_KEY_RIGHT:
-        // add code to change light position
-		cout << "Unhandled key press: right arrow." << endl;
-		break;
-    }
-
-	// this will refresh the screen so that the user sees the light position
-    glutPostRedisplay();
-}
 
 // This function is responsible for displaying the object.
 void drawScene(void)
@@ -105,20 +76,24 @@ void drawScene(void)
 
     // Set material properties of object
 
-	// Here are some colors you might use - feel free to add more
-    GLfloat diffColors[4][4] = { {0.5, 0.5, 0.9, 1.0},
-                                 {0.9, 0.5, 0.5, 1.0},
-                                 {0.5, 0.9, 0.3, 1.0},
-                                 {0.3, 0.8, 0.9, 1.0} };
+    // Here are some colors you might use - feel free to add more
+//    GLfloat diffColors[4][4] = { {0.5, 0.5, 0.9, 1.0},
+//                                 {0.9, 0.5, 0.5, 1.0},
+//                                 {0.5, 0.9, 0.3, 1.0},
+//                                 {0.3, 0.8, 0.9, 1.0}};
+//
+//    // Here we use the first color entry as the diffuse color
+//    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, diffColors[0]);
     
-	// Here we use the first color entry as the diffuse color
-    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, diffColors[0]);
-
-	// Define specular color and shininess
+    float *random_color = update_color_with_a_random_value();
+    GLfloat diffColors[4] = {random_color[0], random_color[1], random_color[2], random_color[3]};
+    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, diffColors);
+    
+    // Define specular color and shininess
     GLfloat specColor[] = {1.0, 1.0, 1.0, 1.0};
     GLfloat shininess[] = {100.0};
 
-	// Note that the specular color and shininess can stay constant
+    // Note that the specular color and shininess can stay constant
     glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specColor);
     glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, shininess);
   
@@ -127,19 +102,68 @@ void drawScene(void)
     // Light color (RGBA)
     GLfloat Lt0diff[] = {1.0,1.0,1.0,1.0};
     // Light position
-	GLfloat Lt0pos[] = {1.0f, 1.0f, 5.0f, 1.0f};
+    GLfloat Lt0pos[] = {1.0f, 1.0f, 5.0f, 1.0f};
 
     glLightfv(GL_LIGHT0, GL_DIFFUSE, Lt0diff);
     glLightfv(GL_LIGHT0, GL_POSITION, Lt0pos);
 
-	// This GLUT method draws a teapot.  You should replace
-	// it with code which draws the object you loaded.
-	glutSolidTeapot(1.0);
+    // This GLUT method draws a teapot.  You should replace
+    // it with code which draws the object you loaded.
+    glutSolidTeapot(1.0);
     
     // Dump the image to the screen.
     glutSwapBuffers();
 
 
+}
+
+// This function is called whenever a "Normal" key press is received.
+void keyboardFunc( unsigned char key, int x, int y )
+{
+    switch ( key )
+    {
+    case 27: // Escape key
+        exit(0);
+        break;
+    case 'c':
+        // add code to change color here
+        glutDisplayFunc( drawScene );
+        cout << "Smash C for no matter how many times you want xD" << key << "." << endl;
+        break;
+    default:
+        cout << "Unhandled key press " << key << "." << endl;
+    }
+
+    // this will refresh the screen so that the user sees the color change
+    glutPostRedisplay();
+}
+
+// This function is called whenever a "Special" key press is received.
+// Right now, it's handling the arrow keys.
+void specialFunc( int key, int x, int y )
+{
+    switch ( key )
+    {
+    case GLUT_KEY_UP:
+        // add code to change light position
+        cout << "Unhandled key press: up arrow." << endl;
+        break;
+    case GLUT_KEY_DOWN:
+        // add code to change light position
+        cout << "Unhandled key press: down arrow." << endl;
+        break;
+    case GLUT_KEY_LEFT:
+        // add code to change light position
+        cout << "Unhandled key press: left arrow." << endl;
+        break;
+    case GLUT_KEY_RIGHT:
+        // add code to change light position
+        cout << "Unhandled key press: right arrow." << endl;
+        break;
+    }
+
+    // this will refresh the screen so that the user sees the light position
+    glutPostRedisplay();
 }
 
 // Initialize OpenGL's rendering modes
@@ -170,7 +194,7 @@ void reshapeFunc(int w, int h)
 
 void loadInput()
 {
-	// load the OBJ file here
+    // load the OBJ file here
 }
 
 // Main routine.
@@ -181,7 +205,7 @@ int main( int argc, char** argv )
 
     glutInit(&argc,argv);
 
-    // We're going to animate it, so double buffer 
+    // We're going to animate it, so double buffer
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH );
 
     // Initial parameters for window position and size
@@ -205,5 +229,5 @@ int main( int argc, char** argv )
     // Start the main loop.  glutMainLoop never returns.
     glutMainLoop( );
 
-    return 0;	// This line is never reached.
+    return 0;    // This line is never reached.
 }
