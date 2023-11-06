04e33c55327880667cb103e2ce1cba935dfbd0a7
SIYAN <siyan.dev@gmail.com>
2023-11-06 18:37:30 +0800

    add fisheye

--- assignment5/distrib/test_cases.sh
+++ assignment5/distrib/test_cases.sh
@@ -4,10 +4,14 @@ rm *.bmp
 
 ./a5 -input scene06_bunny_1k.txt  -size 300 300 -output 6.bmp\
    -shadows -bounces 4 -jitter -filter
+./a5 -input scene06_bunny_1k.txt  -size 300 300 -output 6_no_compression.bmp\
+   -shadows -bounces 4 -jitter
 ./a5 -input scene10_sphere.txt  -size 300 300 -output 10.bmp\
    -shadows -bounces 4 -jitter -filter
 ./a5 -input scene11_cube.txt  -size 300 300 -output 11.bmp\
  -shadows -bounces 4 -jitter -filter
 ./a5 -input scene12_vase.txt  -size 300 300 -output 12.bmp\
  -shadows -bounces 4 -jitter -filter
+./a5 -input scene11_fisheye.txt -size 200 200 -output 11_fisheye.bmp\
+ -shadows -bounces 4 -jitter
 
