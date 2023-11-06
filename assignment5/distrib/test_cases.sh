5d8b5f6f88ac825f43cb19cc41f737c0cf004dfe
SIYAN <siyan.dev@gmail.com>
2023-11-06 19:49:44 +0800

    add wood

--- assignment5/distrib/test_cases.sh
+++ assignment5/distrib/test_cases.sh
@@ -4,6 +4,8 @@ rm *.bmp
 
 ./a5 -input scene06_bunny_1k.txt  -size 300 300 -output 6.bmp\
    -shadows -bounces 4 -jitter -filter -depth 1 100 6_depth.bmp -normal 6_normal.bmp
+./a5 -input scene06_bunny_1k.txt  -size 300 300 -output 6_no_compression.bmp\
+  -shadows -bounces 4 -jitter
 ./a5 -input scene10_sphere.txt  -size 300 300 -output 10.bmp\
    -shadows -bounces 4 -jitter -filter
 ./a5 -input scene11_cube.txt  -size 300 300 -output 11.bmp\
